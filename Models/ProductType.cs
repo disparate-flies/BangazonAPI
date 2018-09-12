71da3e9e8d4a6a15c7d5f438e828ed6efa19a584
Robert Leedy <leedyrj@gmail.com>
2018-09-12 11:59:27 -0500

    added product type model and fixed file names

---
+++ Models/ProductType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductTypes.Models
+{
+    public class ProductType
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+}
