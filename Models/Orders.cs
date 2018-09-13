71d60d02cea91f7048579d5776c762bc2496a601
April Watson <aprilrwat@gmail.com>
2018-09-13 11:56:30 -0500

    Orders model

---
+++ Models/Orders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFBangazon.Models
+{
+    public class Orders
+{
+        public int Id { get; set; }
+        public string OrderDate { get; set; }
+        public Customer Customer { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public List<Product> ProductList { get; set; } = new List<Product>();
+}
+}
