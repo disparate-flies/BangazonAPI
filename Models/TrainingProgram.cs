6cc81846ad74fa3dadabdca8b591689701c4d24c
Robert Leedy <leedyrj@gmail.com>
2018-09-13 11:57:32 -0500

    added body to trainingprogram and did jenn's thing

--- Models/TrainingProgram.cs
+++ Models/TrainingProgram.cs
@@ -7,5 +7,10 @@ namespace DFBangazon.Models
 {
     public class TrainingProgram
     {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee EmployeeId { get; set; }
+        public int TrainingId { get; set; }
+        public Training TrainingId { get; set; }
     }
 }
