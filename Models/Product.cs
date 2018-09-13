ec10d9a27fe3cf66075c45a0bfcd9d205f533fdc
Jenn Lawson <jenniferlawson000@gmail.com>
2018-09-13 09:46:35 -0500

    created Product Model

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFBangazon.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Title { get; set;}
+        public string ProductDesc { get; set; }
+        public int Quantity { get; set; }
+        public Customer CustomerId { get; set;}
+        public ProductType ProductTypeId { get; set; }
+    }
+}
