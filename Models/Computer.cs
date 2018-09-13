703c3542be8aa9686e55cfd47fac6754d832538c
sathvikreddy615 <sathvik.reddy615@gmail.com>
2018-09-13 14:11:54 -0500

    created Computer model

---
+++ Models/Computer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFBangazon.Models
+{
+    public class Computer
+    {
+        public int Id;
+        public string PurchaseDate;
+        public string Condition;
+        public string DecomissionDate;
+    }
+}
