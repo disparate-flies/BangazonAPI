24d25e10cb677827004b4e97df93cb5fcc5c27f5
sathvikreddy615 <sathvik.reddy615@gmail.com>
2018-09-13 14:26:02 -0500

    added a List for Employees and added get / set

--- Models/Computer.cs
+++ Models/Computer.cs
@@ -7,9 +7,10 @@ namespace DFBangazon.Models
 {
     public class Computer
     {
-        public int Id;
-        public string PurchaseDate;
-        public string Condition;
-        public string DecomissionDate;
+        public int Id { get; set; }
+        public string PurchaseDate { get; set; }
+        public string Condition { get; set; }
+        public string DecomissionDate { get; set; }
+        List<Employee> EmployeeList = new List<Employee>;
     }
 }
