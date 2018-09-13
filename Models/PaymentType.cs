d860addf1f2f470c8a0eb9beaca67b45bac9953a
sathvikreddy615 <sathvik.reddy615@gmail.com>
2018-09-13 11:33:52 -0500

    completed first draft of model for PaymentType

---
+++ Models/PaymentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFBangazon.Models
+{
+    public class PaymentType
+{
+    public int AccountNo { get; set; }
+    public string type { get; set; }
+    public string Nickname { get; set; }
+    public Customer Customer { get; set; }
+}
+}
