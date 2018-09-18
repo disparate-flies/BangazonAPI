10cbe38a32fa7aa0e6c89625fca486f9c487414a
Jenn Lawson <jenniferlawson000@gmail.com>
2018-09-18 11:57:52 -0500

    working on get method

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -11,6 +11,7 @@ namespace DFBangazon.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public bool IsSupervisor { get; set; }
+        public bool IsActive { get; set; }
         public int DepartmentId { get; set;}
         public Department Department { get; set; }
         public Computer Computer { get; set; }
